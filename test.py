3b30a00fe871942f6754d5c8a715fa764b636a86
Nick Geoca <nickgeoca@gmail.com>
2016-08-23 23:09:03 -0500

    Added test file

---
+++ test.py
@@ -0,0 +1,19 @@
+import nonogram_sovler
+
+col = [ []
+      , []
+      , [1]
+      ]
+
+row = [ []
+      , [1]
+      ]
+
+
+# Ways to test.
+#  1. Randomly generate problems. Validate solutions
+#  2. Set of problems. Validate solutions.
+#  3. Set of problems. Known solutions == solver solutions?
+
+solution = solve_engine()
+
